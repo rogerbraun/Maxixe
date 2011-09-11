8422697922b89a99535b950f2bf94edcb135d586
Roger Braun <davinelulinvega@gmail.com>
2011-09-11 23:02:45 +0200

    cattier tests

--- spec/trainer/trainer_spec.rb
+++ spec/trainer/trainer_spec.rb
@@ -12,17 +12,20 @@ describe Maxixe::Trainer do
 
   it "should be able to find the optimal threshold and n values" do
     pre_segmented = [["MYDOGISINTHEHOUSE", "MY DOG IS IN THE HOUSE"],
-                     ["FOURNICEDOGS", "FOUR NICE DOGS"]]  
+                     ["FOURNICEDOGS", "FOUR NICE DOGS"],
+                     ["MYCATLIKESMYDOG", "MY CAT LIKES MY DOG"]]
     index = Maxixe::Trainer.generate_corpus_from_io([2,3,4,5], "ILIKEMYDOG
 THISHOUSEISMYHOUSE
 MYDOGISSONICE
+WHOLIKESDOGSANYWAY
+CATSANDDOGSUSUALLYFIGHT
 INMYHOUSETHEREAREFOURDOGS
 IWANTAHOUSEFORMYDOG")
   
     optimal = Maxixe::Trainer.optimize(index, pre_segmented)
-    optimal[:n].should == ["3"]
+    optimal[:n].should == ["2","4"]
     optimal[:score].should == 0
-    optimal[:t].should be_within(0.01).of(0.3)
+    optimal[:t].should be_within(0.01).of(0.5)
 
   end
 
